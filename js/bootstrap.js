1eeca7abed854b134bbb6169bd72c039b2d00d31
太爱喝牛奶 <1016425964@qq.com>
2016-08-03 13:16:34 +0800

    。

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2,18 +2,22 @@ setTimeout(function(){
     $(document).getElementsByTagName('body')[0].style.height=window.innerHeight+'px';
 },20);
 
-setTimeout(loadMusic, 20000);
+
+
 function loadMusic() {
     if ($('#music-content')) {
-        $('#music-content').append('<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=450 src="http://music.163.com/outchain/player?type=0&id=49176408&auto=0&height=430"></iframe>')
+        $('#music-content').append('<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=450 src="http://music.163.com/outchain/player?type=0&id=435878441&auto=1&height=430"></iframe>');
     }
-
 }
-$(document).ready(function(){
-    $('$music-control').click(function(){
-        $('$music-content').toggle()
-    })
-});
+function toggleMusicPanel(){
+    $('#music-content').toggle()
+}
+loadMusic();
+//function toggleMusicPanel(){
+//    $('#music-control').onclick(function(){
+//        $('#music-content').toggle(loadMusic(),500)
+//    })
+//}
 //var isFirstToggleMusic = true;
 //function toggleMusicPanel() {
 //    $('#music-control').toggleClass('on');
